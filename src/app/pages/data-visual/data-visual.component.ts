a35d7595fb29df6d8ac68abd461a69fa08300b0e
John Kenway <waldron@JohndeMBP.lan>
2020-10-15 23:34:11 +0800

    PCA

--- src/app/pages/data-visual/data-visual.component.ts
+++ src/app/pages/data-visual/data-visual.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js/dist/Chart';
 import * as line from '../../../assets/myLogisticRegression1.json';
-import * as iris from "../../../assets/iris_processed.json";
-import { ScatterDataItem } from '../../types/scatter-data'
+import * as iris from "../../../assets/iris_processed_PCA.json";
+import { ScatterDataItem } from '../../types/scatter-data';
 
 @Component({
   selector: 'app-data-visual',
@@ -24,22 +24,21 @@ export class DataVisualComponent implements OnInit {
 
   ngOnInit(): void {
     // 读iris数据集
-    let tagMap = new Map();
-    tagMap.set("Iris-setosa", 1);
-    tagMap.set("Iris-versicolor", 0);
-    let tempData = (iris as any).default as string[];
-    tempData.forEach((val: string, index: number) => {
-      let tokens = val.split(',', 5);
+    let irisPreProcessed = (iris as any).default as {
+      data: number[][];
+      tags: number[];
+    };
+    for(let i = 0; i < irisPreProcessed.data[0].length; i++) {
       this.irisData.push({
-        x: Number(tokens[0]),
-        y: Number(tokens[1]),
-        label: tagMap.get(tokens[4])
+        x: irisPreProcessed.data[0][i],
+        y: irisPreProcessed.data[1][i],
+        label: Boolean(irisPreProcessed.tags[i])
       });
-    });
+    }
 
     // 取分界线上的两个点
-    let x1 = {x: 4, y: -(this.lineData[0][0]*4 + line[2][0])/line[1][0]};
-    let x2 = {x:7, y: -(this.lineData[0][0]*7 + line[2][0])/line[1][0]};
+    let x1 = {x: -(this.lineData[1][0]*(1.5) + this.lineData[2][0])/this.lineData[0][0], y: 1.5};
+    let x2 = {x: -(this.lineData[1][0]*(-1.5) + this.lineData[2][0])/this.lineData[0][0], y: -1.5};
     this.dividingLine.push(x1);
     this.dividingLine.push(x2); 
   }
@@ -79,12 +78,21 @@ export class DataVisualComponent implements OnInit {
         scales: {
           xAxes: [{
             type: 'linear',
-            position: 'bottom'
+            position: 'bottom',
+            scaleLabel: {
+              labelString: "First Principle Component",
+              display: true
+            }
+          }],
+          yAxes: [{
+            type: 'linear',
+            position: 'left',
+            scaleLabel: {
+              labelString: "Second Principle Component",
+              display: true
+            }
           }]
         },
-        points: {
-
-        }
       }
     });
   }
